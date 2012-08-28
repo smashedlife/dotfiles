cb6dcd7fa63ab24e206d5af9adf537804d3b96c4
Jonathan So <jso.life@gmail.com>
2012-08-27 21:29:56 -0700

    Added sh file and gitignore

---
+++ bootstrap_dotfiles.sh
@@ -0,0 +1,11 @@
+ln -s ~/dotfiles/.bash_profile ~/.bash_profile
+ln -s ~/dotfiles/.gemrc ~/.gemrc
+ln -s ~/dotfiles/.git_completion.sh ~/.git_completion.sh
+ln -s ~/dotfiles/.gitconfig ~/.gitconfig
+ln -s ~/dotfiles/.gitignore_global ~/.gitignore_global
+ln -s ~/dotfiles/.gvimrc ~/.gvimrc
+ln -s ~/dotfiles/.vim ~/.vim
+ln -s ~/dotfiles/.vimrc ~/.vimrc
+
+mkdir ~/.vimbackups
+
