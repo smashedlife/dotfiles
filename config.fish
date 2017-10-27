b159f7af72716ad9a04671501e990cfaecd8ea69
Jonathan So <jso.life@gmail.com>
2017-10-26 23:08:11 -0700

    Add fish and modify tmux.conf

---
+++ config.fish
@@ -0,0 +1,19 @@
+export EDITOR="nvim"
+export BUNDLER_EDITOR="nvim"
+
+alias vim="nvim"
+alias showhiddenfiles='defaults write com.apple.finder AppleShowAllFiles TRUE'
+alias hidehiddenfiles='defaults write com.apple.finder AppleShowAllFiles FALSE'
+alias pythonwebserver='python -m SimpleHTTPServer'
+
+alias gst='git status'
+alias gdw='git diff --word-diff'
+alias gl='git log --oneline'
+alias gt='git log --graph --oneline --decorate'
+
+alias dev='cd ~/Projects'
+alias weather='curl -4 wttr.in/Portland'
+
+if test -f ~/.config/fish/config.fish.local
+  source ~/.config/fish/config.fish.local
+end
