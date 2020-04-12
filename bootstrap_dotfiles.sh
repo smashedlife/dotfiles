5efe3ac913d247a380cf7edc761ff32bdf8f1a89
Jonathan So <jso.life@gmail.com>
2020-04-12 14:40:27 -0700

    Change bootstrap file for new configurations

--- bootstrap_dotfiles.sh
+++ bootstrap_dotfiles.sh
@@ -7,10 +7,12 @@ ln -fs $PWD/gvimrc ~/.gvimrc
 ln -fs $PWD/vimrc ~/.vimrc
 ln -fs $PWD/dir_colors ~/.dir_colors
 ln -fs $PWD/tmux.conf ~/.tmux.conf
+ln -fs $PWD/tmux ~/.tmux
 ln -fs $PWD/zsh ~/.zsh
+ln -fs $PWD/hyper.js ~/.hyper.js
 ln -fs $PWD/zshrc ~/.zshrc
 ln -fs $PWD/zsh_alias ~/.zsh_alias
 ln -fs $PWD/zprofile ~/.zprofile
 
-mkdir ~/.vimbackups
 
+mkdir ~/.vimbackups
